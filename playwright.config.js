7662d7099ba7198d9c5d7a2a240a6ca5e8e3da16
Luciano <lof.fagundes@gmail.com>
2026-09-24 16:47:25 -0300

    chore: configura eslint no lefthook

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,5 @@
 // @ts-check
 const { defineConfig, devices } = require("@playwright/test");
-const { channel } = require("node:diagnostics_channel");
 
 module.exports = defineConfig({
   testDir: "./tests",
