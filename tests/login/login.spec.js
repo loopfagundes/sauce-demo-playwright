79571abe11ece7c0d47d58f334a85ccfa1d5e056
Luciano <lof.fagundes@gmail.com>
2026-09-25 17:41:02 -0300

    refactor: login tests

--- tests/login/login.spec.js
+++ tests/login/login.spec.js
@@ -1,8 +1,7 @@
-const { test, expect } = require('../../fixtures/basesTests/base.fixture');
+const { test, expect } = require("../../fixtures/basesTests/base.fixture");
 const usuario = require("../../fixtures/usuarios.json");
 const testData = require("../../fixtures/testData.json");
 const urls = require("../../fixtures/urlTest.json");
-
 
 test.describe("Login", () => {
   test("has title", async ({ page }) => {
@@ -27,30 +26,28 @@ test.describe("Login", () => {
 
   test("login com credenciais bloqueado", async ({ login }) => {
     await login.goto();
-    await login.login(
-      usuario.bloqueado.usuario,
-      usuario.bloqueado.senha
-    );
+    await login.login(usuario.bloqueado.usuario, usuario.bloqueado.senha);
 
     await expect(login.getErrorMessage()).toBeVisible();
   });
 
   test("login com usuário problemático", async ({ page, login, inventory }) => {
     await login.goto();
-    await login.login(
-      usuario.problematico.usuario,
-      usuario.problematico.senha
-    );
+    await login.login(usuario.problematico.usuario, usuario.problematico.senha);
 
     await expect(page).toHaveURL(urls.inventory);
     await expect(inventory.getTitle()).toHaveText(testData.title.products);
   });
 
-  test("login com usuário performance glitch", async ({ page, login, inventory }) => {
+  test("login com usuário performance glitch", async ({
+    page,
+    login,
+    inventory,
+  }) => {
     await login.goto();
     await login.login(
       usuario.performanceGlitch.usuario,
-      usuario.performanceGlitch.senha
+      usuario.performanceGlitch.senha,
     );
 
     await expect(page).toHaveURL(urls.inventory);
@@ -59,10 +56,7 @@ test.describe("Login", () => {
 
   test("login com usuário error", async ({ page, login, inventory }) => {
     await login.goto();
-    await login.login(
-      usuario.error.usuario,
-      usuario.error.senha
-    );
+    await login.login(usuario.error.usuario, usuario.error.senha);
 
     await expect(page).toHaveURL(urls.inventory);
     await expect(inventory.getTitle()).toHaveText(testData.title.products);
@@ -70,13 +64,9 @@ test.describe("Login", () => {
 
   test("login com usuário visual", async ({ page, login, inventory }) => {
     await login.goto();
-    await login.login(
-      usuario.visual.usuario,
-      usuario.visual.senha
-    );
+    await login.login(usuario.visual.usuario, usuario.visual.senha);
 
     await expect(page).toHaveURL(urls.inventory);
     await expect(inventory.getTitle()).toHaveText(testData.title.products);
   });
-
 });
